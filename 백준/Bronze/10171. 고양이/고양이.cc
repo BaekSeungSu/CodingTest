96b935e1a07c3ad89e3919db22e764f6cab60750
BaekSeungSu <91333092+BaekSeungSu@users.noreply.github.com>
2025-05-19 15:30:36 +0900

    [Bronze V] Title: 고양이, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10171. 고양이/고양이.cc
@@ -0,0 +1,13 @@
+// 백준 10171번 : 고양이
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+
+	cout << "\\    /\\" << "\n" << " )  ( ')" << "\n" << "(  /  )" << "\n" << " \\(__)|";
+	return 0;
+}

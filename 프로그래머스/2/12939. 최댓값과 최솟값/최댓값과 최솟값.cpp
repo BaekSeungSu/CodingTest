79f0a4e503f9ecd2ae3755ae629aeea2394e3176
BaekSeungSu <91333092+BaekSeungSu@users.noreply.github.com>
2025-05-27 11:15:48 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 0.04 ms, Memory: 4.14 MB -BaekjoonHub

---
+++ 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.cpp
@@ -0,0 +1,36 @@
+#include <string>
+#include <vector>
+#include <sstream>
+#include <iostream>
+#include <algorithm>>
+using namespace std;
+
+string solution(string s) {
+    string answer = "";
+    vector<int> a;
+    vector<string> words;
+    stringstream ss(s);
+
+    string word;
+    while (getline(ss, word, ' ')) {
+        words.push_back(word);
+    }
+
+    for (int i = 0; i < words.size(); i++) {
+        a.push_back(stoi(words[i]));
+    }
+
+    int max = *max_element(a.begin(), a.end());
+    int min = *min_element(a.begin(), a.end());
+    
+    answer = to_string(min) + ' ' + to_string(max);
+
+    return answer;
+}
+
+int main() {
+    string a;
+    a = solution("1 2 3 4");
+
+    cout << a << endl;
+}

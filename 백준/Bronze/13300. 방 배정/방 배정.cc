d5b3c137e49504b01ee4a1509d57d24da177875b
BaekSeungSu <91333092+BaekSeungSu@users.noreply.github.com>
2025-06-03 19:03:37 +0900

    [Bronze II] Title: 방 배정, Time: 0 ms, Memory: 2020 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/13300. 방 배정/방 배정.cc
+++ 백준/Bronze/13300. 방 배정/방 배정.cc
@@ -1,29 +1,29 @@
-// 백준 13300 : 방 배정
-
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-int N, K, cnt;
-int arr[12];
+int N, K, ans=0;
+int s[2][7]={};  // 성별/반별 학생 수를 저장하는 배열
 
-int main()
-{
-	cin >> N >> K;
+int main(void){
+  ios::sync_with_stdio(0);
+  cin.tie(0);
 
-	for (int i = 0; i < N; i++)
-	{
-		int S, Y;
-		cin >> S >> Y;
+  cin >> N >> K;
+  // 학생 수 저장
+  for (int i=0; i<N; ++i) {
+    int a, b;
+    cin >> a >> b;
+    s[a][b]++;
+  }
 
-		if (S == 0) arr[Y - 1]++;
-		else arr[Y + 5]++;
-	}
-
-	for (int i = 0; i < 12; i++)
-	{
-		if (arr[i] != 0 && arr[i] <= K) cnt++;
-		if (arr[i] > K) cnt += arr[i] / K + 1;
-	}
-
-	cout << cnt;
+  // 필요한 방의 개수 계산
+  for (int i=0; i<2; ++i) {
+    for (int j=1; j<7; ++j) {
+      // 배정에 필요한 만큼 방의 개수 추가
+      ans += s[i][j] / K;
+      // 학생이 남을 경우, 하나의 방이 더 필요하므로 방의 개수 추가
+      if (s[i][j] % K) ++ans;
+    }
+  }
+  cout << ans;
 }

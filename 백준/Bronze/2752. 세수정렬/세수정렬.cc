d90438fb22af8bd11d9fd6f2ddb29537c9b016a9
BaekSeungSu <91333092+BaekSeungSu@users.noreply.github.com>
2025-06-30 12:30:00 +0900

    [Bronze IV] Title: 세수정렬, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2752. 세수정렬/세수정렬.cc
+++ 백준/Bronze/2752. 세수정렬/세수정렬.cc
@@ -1,36 +1,17 @@
-// 백준 2752번 : 세수정렬
 #include<bits/stdc++.h>
-
 using namespace std;
 
 int main()
 {
 	ios::sync_with_stdio(false);
-	cin.tie(NULL); cout.tie(NULL);
+	cin.tie(nullptr), cout.tie(nullptr);
 
 	int A, B, C;
 	cin >> A >> B >> C;
+	int D, E, F;
+	D = min({ A, B, C });
+	F = max({ A, B, C });
+	E = A + B + C - D - F;
+	cout << D << ' ' << E << ' ' << F;
 
-	if (A < B && A < C) {
-		if (B < C)
-			cout << A << " " << B << " " << C;
-		else
-			cout << A << " " << C << " " << B;
-	}
-	else if (B < A && B < C) {
-		if (A < C)
-			cout << B << " " << A << " " << C;
-		else
-			cout << B << " " << C << " " << A;
-	}
-	else {
-		if (A < B)
-			cout << C << " " << A << " " << B;
-		else
-			cout << C << " " << B << " " << A;
-	}
-	
-
-
-	return 0;
 }

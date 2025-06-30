9dc0bf286010bef254a3536e0ae7f98ca2be9b21
BaekSeungSu <91333092+BaekSeungSu@users.noreply.github.com>
2025-06-30 11:23:54 +0900

    [Bronze V] Title: 사칙연산, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/10869. 사칙연산/사칙연산.cc
+++ 백준/Bronze/10869. 사칙연산/사칙연산.cc
@@ -1,16 +1,13 @@
-// 백준 10869번 : 사칙연산
 #include<bits/stdc++.h>
-
 using namespace std;
 
 int main()
 {
 	ios::sync_with_stdio(false);
-	cin.tie(NULL); cout.tie(NULL);
+	cin.tie(nullptr), cout.tie(nullptr);
 
 	int A, B;
 	cin >> A >> B;
-	cout << A + B << "\n" << A - B << "\n" << A * B << "\n" << A / B << "\n" << A % B;
 
-	return 0;
+	cout << A + B << '\n' << A - B << '\n' << A * B << '\n' << A / B << '\n' << A % B;
 }

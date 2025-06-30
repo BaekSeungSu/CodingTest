626dce31938ca140e7f6ad947e884b451443a3ad
BaekSeungSu <91333092+BaekSeungSu@users.noreply.github.com>
2025-06-30 15:08:13 +0900

    [Bronze III] Title: 핸드폰 요금, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/1267. 핸드폰 요금/핸드폰 요금.cc
+++ 백준/Bronze/1267. 핸드폰 요금/핸드폰 요금.cc
@@ -1,44 +1,35 @@
-// 백준 1267번 : 핸드폰 요금
 #include<bits/stdc++.h>
-
 using namespace std;
 
 int main()
 {
 	ios::sync_with_stdio(false);
-	cin.tie(NULL); cout.tie(NULL);
+	cin.tie(nullptr), cout.tie(nullptr);
 
-	int N, total_Y = 0, total_M = 0;
+	// 영식 30초 10
+	// 민식 60초 15
+
+	int N, Young = 0, Min = 0;
 	cin >> N;
 
-	vector<int> call(N);
-
 	for (int i = 0; i < N; i++)
 	{
-		cin >> call[i];
+		int time;
+		cin >> time;
+		Young += (time / 30 + 1) * 10;
+		Min += (time / 60 + 1) * 15;
 	}
 
-	for (int i = 0; i < N; i++)
+	if (Min > Young)
 	{
-		int Y, M;
-		Y = (call[i] / 30 + 1) * 10;
-		M = (call[i] / 60 + 1) * 15;
-
-		total_Y += Y;
-		total_M += M;
+		cout << "Y " << Young;
 	}
-
-	if (total_Y == total_M)
+	else if (Min < Young)
 	{
-		cout << "Y M" << " " << total_Y;
+		cout << "M " << Min;
 	}
-	else if (total_Y > total_M)
+	else
 	{
-		cout << "M " << total_M;
+		cout << "Y M " << Young;
 	}
-	else {
-		cout << "Y " << total_Y;
-	}
-
-	return 0;
 }

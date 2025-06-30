41cbc065629dc249790fa56951566c159f0c3f64
BaekSeungSu <91333092+BaekSeungSu@users.noreply.github.com>
2025-06-30 19:34:42 +0900

    [Silver V] Title: 방 번호, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/1475. 방 번호/방 번호.cc
+++ 백준/Silver/1475. 방 번호/방 번호.cc
@@ -1,35 +1,28 @@
-// 백준 1475 : 방 번호
-
 #include<bits/stdc++.h>
 using namespace std;
+
+int arr[10];
 
 int main()
 {
 	ios::sync_with_stdio(false);
-	cin.tie(NULL); cout.tie(NULL);
+	cin.tie(nullptr); cout.tie(nullptr);
 
 	string s;
-
 	cin >> s;
 
-	int Arr[10] = {};
-
-	for (int i = 0; i < s.size(); i++)
+	for (char c : s)
 	{
-		Arr[s[i] - '0']++;
+		arr[c - '0']++;
 	}
 
-	if ((Arr[6] + Arr[9]) % 2 == 1)
+	int result = 0;
+	for (int i = 0; i < 10; i++)
 	{
-		Arr[6] = (Arr[6] + Arr[9]) / 2 + 1;
-		Arr[9] = Arr[6];
+		if (i == 6 || i == 9) continue;
+		result = max(arr[i], result);
 	}
-	else {
-		Arr[6] = (Arr[6] + Arr[9]) / 2;
-		Arr[9] = Arr[6];
-	}
+	result = max(result, (arr[6] + arr[9] + 1) / 2);
 
-	cout << *max_element(Arr, Arr + 10);
-
-	return 0;
+	cout << result;
 }

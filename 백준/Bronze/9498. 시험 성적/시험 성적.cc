1aadfa205a9ad0aa70aa699930303160afed5dac
BaekSeungSu <91333092+BaekSeungSu@users.noreply.github.com>
2025-06-30 11:25:56 +0900

    [Bronze V] Title: 시험 성적, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/9498. 시험 성적/시험 성적.cc
+++ 백준/Bronze/9498. 시험 성적/시험 성적.cc
@@ -1,35 +1,32 @@
-// 백준 9498번 : 시험 성적
 #include<bits/stdc++.h>
-
 using namespace std;
 
 int main()
 {
 	ios::sync_with_stdio(false);
-	cin.tie(NULL); cout.tie(NULL);
+	cin.tie(nullptr), cout.tie(nullptr);
 
 	int score;
 	cin >> score;
 
-	if (score <= 100 && score >= 90) {
-		cout << "A";
-	}
-	else if(score >= 80)
+	if (score >= 90)
 	{
-		cout << "B";
+		cout << 'A';
+	}
+	else if (score >= 80)
+	{
+		cout << 'B';
 	}
 	else if (score >= 70)
 	{
-		cout << "C";
+		cout << 'C';
 	}
 	else if (score >= 60)
 	{
-		cout << "D";
+		cout << 'D';
 	}
 	else
 	{
-		cout << "F";
+		cout << 'F';
 	}
-
-	return 0;
 }

b84bf7e8171ffda7c91e2625414ea26220a45274
BaekSeungSu <91333092+BaekSeungSu@users.noreply.github.com>
2025-06-30 16:42:44 +0900

    [Bronze IV] Title: 빠른 A+B, Time: 192 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/15552. 빠른 A＋B/빠른 A＋B.cc
+++ 백준/Bronze/15552. 빠른 A＋B/빠른 A＋B.cc
@@ -1,20 +1,18 @@
-// 백준 15552번 : 빠른 A+B
 #include<bits/stdc++.h>
-
 using namespace std;
 
 int main()
 {
 	ios::sync_with_stdio(false);
-	cin.tie(NULL); cout.tie(NULL);
+	cin.tie(nullptr), cout.tie(nullptr);
 
-	int T, a, b;
+	int T;
 	cin >> T;
+
 	for (int i = 0; i < T; i++)
 	{
-		cin >> a >> b;
-		cout << a + b << "\n";
+		int A, B;
+		cin >> A >> B;
+		cout << A + B << '\n';
 	}
-
-	return 0;
 }

c78e3296b6312de3213f460226e557c2029a6982
BaekSeungSu <91333092+BaekSeungSu@users.noreply.github.com>
2025-07-08 11:09:39 +0900

    [Silver III] Title: N과 M (2), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.cc
@@ -0,0 +1,42 @@
+// 백준 15650 : N과 M(2)
+#include<bits/stdc++.h>
+using namespace std;
+
+int n, m;
+int arr[10];
+
+void func(int k)
+{
+	if (k == m)
+	{
+		for (int i = 0; i < m; i++)
+		{
+			cout << arr[i] << ' ';
+		}
+		cout << '\n';
+		return;
+	}
+
+	int st = 1;
+	if (k != 0)
+	{
+		st = arr[k - 1] + 1;
+	}
+
+	for (int i = st; i <= n; i++)
+	{
+		arr[k] = i;
+		func(k + 1);
+	}
+}
+
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr); cout.tie(nullptr);
+	
+	cin >> n >> m;
+	func(0);
+
+}

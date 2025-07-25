25dabdfe0b491db78bf598a36fe84b2a3ffb0125
BaekSeungSu <91333092+BaekSeungSu@users.noreply.github.com>
2025-07-25 12:25:20 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.cpp
+++ 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.cpp
@@ -1,17 +1,26 @@
 #include <string>
 #include <vector>
-
+#include <iostream>
 using namespace std;
 
 bool solution(string s) {
     bool answer = false;
     if (s.size() == 4 || s.size() == 6)
-        for (int i = 0; i < s.size(); i++) {
-            if (isdigit(s[i])) answer = true;
-            else {
+    {
+        for (int i = 0; i < s.size(); i++) 
+        {
+            if (isdigit(s[i])) 
+            {
+                answer = true;
+            }
+            else 
+            {
                 answer = false;
                 break;
             }
         }
+    }
+    
+    
     return answer;
 }

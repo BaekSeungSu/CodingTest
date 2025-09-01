81fb6801cc9e713bb0c44f1b0cf88cb50a4027e7
BaekSeungSu <91333092+BaekSeungSu@users.noreply.github.com>
2025-09-01 09:16:16 +0900

    [Silver IV] Title: 큐 2, Time: 324 ms, Memory: 10424 KB -BaekjoonHub

--- 백준/Silver/18258. 큐 2/큐 2.cc
+++ 백준/Silver/18258. 큐 2/큐 2.cc
@@ -1,90 +1,66 @@
 // 백준 18258 : 큐 2
-
 #include<bits/stdc++.h>
-
 using namespace std;
-
-const long long MX = 2000001;
-long long dat[MX];
-long long head = 0, tail = 0;
-
-void push(int x)
-{
-	dat[tail] = x;
-	tail++;
-}
-
-void pop()
-{
-	if (head == tail) cout << "-1" << "\n";
-	else
-	{
-		cout << dat[head] << "\n";
-		head++;
-	}
-}
-
-void size()
-{
-	cout << tail - head << "\n";
-}
-
-void empty()
-{
-	if (head == tail) cout << "1" << "\n";
-	else cout << "0" << "\n";
-}
-
-void front()
-{
-	if (head == tail) cout << "-1" << "\n";
-	else cout << dat[head] << "\n";
-}
-
-void back()
-{
-	if (head == tail) cout << "-1" << "\n";
-	else cout << dat[tail - 1] << "\n";
-}
 
 int main()
 {
 	ios::sync_with_stdio(false);
-	cin.tie(NULL); cout.tie(NULL);
-
-	int N;
-	cin >> N;
-
-	for (int i = 0; i < N; i++)
-	{
-		string s;
-		cin >> s;
-
-		if (s == "push")
-		{
-			int x;
-			cin >> x;
-			push(x);
-		}
-		else if (s == "pop")
-		{
-			pop();
-		}
-		else if (s == "size")
-		{
-			size();
-		}
-		else if (s == "empty")
-		{
-			empty();
-		}
-		else if (s == "front")
-		{
-			front();
-		}
-		else if (s == "back")
-		{
-			back();
-		}
-	}
+	cin.tie(nullptr); cout.tie(nullptr);
+    
+    queue<int> q;
+    int n;
+    cin >> n;
+    while (n--) 
+    {
+        string s;
+        cin >> s;
+        if (s == "push") 
+        {
+            int val;
+            cin >> val;
+            q.push(val);
+        }
+        else if (s == "pop") 
+        {
+            if (q.empty())
+            {
+                cout << -1 << '\n';
+            }
+            else 
+            {
+                cout << q.front() << '\n';
+                q.pop();
+            }
+        }
+        else if (s == "size") 
+        {
+            cout << q.size() << '\n';
+        }
+        else if (s == "empty") 
+        {
+            cout << q.empty() << '\n';
+        }
+        else if (s == "front") 
+        {
+            if (q.empty())
+            {
+                cout << -1 << '\n';
+            }
+            else 
+            {
+                cout << q.front() << '\n';
+            }
+        }
+        else 
+        {
+            if (q.empty())
+            {
+                cout << -1 << '\n';
+            }
+            else
+            {
+                cout << q.back() << '\n';
+            }
+        }
+    }
 }

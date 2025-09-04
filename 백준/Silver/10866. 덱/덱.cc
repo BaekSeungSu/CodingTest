526405150171009b3253ba7a9c8feac37c1a3a00
BaekSeungSu <91333092+BaekSeungSu@users.noreply.github.com>
2025-09-04 13:28:46 +0900

    [Silver IV] Title: 덱, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/10866. 덱/덱.cc
+++ 백준/Silver/10866. 덱/덱.cc
@@ -1,115 +1,94 @@
 // 백준 10866 : 덱
-
 #include<bits/stdc++.h>
-
 using namespace std;
-
-const int MX = 1000005;
-int dat[2*MX + 1];
-int head = MX, tail = MX;
-
-void push_front(int x)
-{
-	dat[--head] = x;
-}
-
-void push_back(int x)
-{
-	dat[tail++] = x;
-}
-
-void pop_front()
-{
-	if (head == tail) cout << "-1" << "\n";
-	else
-	{
-		cout << dat[head] << "\n";
-		head++;
-	}
-}
-
-void pop_back()
-{
-	if (head == tail) cout << "-1" << "\n";
-	else
-	{
-		cout << dat[tail-1] << "\n";
-		tail--;
-	}
-}
-
-void size()
-{
-	cout << tail - head << "\n";
-}
-
-void empty()
-{
-	if (head == tail) cout << "1" << "\n";
-	else cout << "0" << "\n";
-}
-
-void front()
-{
-	if (head == tail) cout << "-1" << "\n";
-	else cout << dat[head] << "\n";
-}
-
-void back()
-{
-	if (head == tail) cout << "-1" << "\n";
-	else cout << dat[tail - 1] << "\n";
-}
 
 int main()
 {
 	ios::sync_with_stdio(false);
-	cin.tie(NULL); cout.tie(NULL);
+	cin.tie(nullptr); cout.tie(nullptr);
 
-	int N;
-	cin >> N;
+	int n;
+	cin >> n;
 
-	for (int i = 0; i < N; i++)
+	deque<int> d;
+	
+	for (int i = 0; i < n; i++)
 	{
 		string s;
 		cin >> s;
-
-		if (s == "push_back")
+		if (s == "push_front")
 		{
-			int x;
-			cin >> x;
-			push_back(x);
+			int k;
+			cin >> k;
+			d.push_front(k);
 		}
-		else if (s == "push_front")
+		else if (s == "push_back")
 		{
-			int x;
-			cin >> x;
-			push_front(x);
+			int k;
+			cin >> k;
+			d.push_back(k);
 		}
 		else if (s == "pop_front")
 		{
-			pop_front();
+			if (d.empty())
+			{
+				cout << -1 << '\n';
+			}
+			else
+			{
+				cout << d.front() << '\n';
+				d.pop_front();
+			}
 		}
 		else if (s == "pop_back")
 		{
-			pop_back();
+			if (d.empty())
+			{
+				cout << -1 << '\n';
+			}
+			else
+			{
+				cout << d.back() << '\n';
+				d.pop_back();
+			}
 		}
 		else if (s == "size")
 		{
-			size();
+			cout << d.size() << '\n';
 		}
 		else if (s == "empty")
 		{
-			empty();
+			if (d.empty())
+			{
+				cout << 1 << '\n';
+			}
+			else
+			{
+				cout << 0 << '\n';
+			}
 		}
 		else if (s == "front")
 		{
-			front();
+			if (d.empty())
+			{
+				cout << -1 << '\n';
+			}
+			else
+			{
+				cout << d.front() << '\n';
+			}
 		}
-		else if (s == "back")
+		else
 		{
-			back();
+			if (d.empty())
+			{
+				cout << -1 << '\n';
+			}
+			else
+			{
+				cout << d.back() << '\n';
+			}
 		}
 	}
-	return 0;
+
 }

79f954b31dfe0a59b3bfb9d790ccab07af87c9c0
BaekSeungSu <91333092+BaekSeungSu@users.noreply.github.com>
2025-09-08 10:46:33 +0900

    [Silver II] Title: 쇠막대기, Time: 0 ms, Memory: 2300 KB -BaekjoonHub

--- 백준/Silver/10799. 쇠막대기/쇠막대기.cc
+++ 백준/Silver/10799. 쇠막대기/쇠막대기.cc
@@ -1,39 +1,37 @@
-// 백준 10799 : 쇠막대기
-
+// 백준 10799 : 쇠막대기 
 #include<bits/stdc++.h>
 using namespace std;
-
 
 int main()
 {
 	ios::sync_with_stdio(false);
-	cin.tie(NULL); cout.tie(NULL);
+	cin.tie(nullptr); cout.tie(nullptr);
 
-	string s;
-	cin >> s;
+	string p;
+	cin >> p;
 
-	stack<char> myStack;
-	int count = 0;
+	stack<char> s;
+	int ans = 0;
 
-	for (int i = 0; i < s.size(); i++)
+	for (int i = 0; i < p.size(); i++)
 	{
-		if (s[i] == '(')
+		if (p[i] == '(')
 		{
-			myStack.push(s[i]);
+			s.push(p[i]);
 		}
-		else // s[i] == ')'
+		else // ')'
 		{
-			if (s[i - 1] == '(') {
-				myStack.pop();
-				count += myStack.size();
+			if (p[i - 1] == '(')
+			{
+				s.pop();
+				ans += s.size();
 			}
-			else {
-				myStack.pop();
-				count++;
+			else //')'
+			{
+				s.pop();
+				ans++;
 			}
 		}
 	}
-	cout << count;
-
-	return 0;
+	cout << ans;
 }

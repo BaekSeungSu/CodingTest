48528cd2c321a7346620b02a5569ef2952e859e5
BaekSeungSu <91333092+BaekSeungSu@users.noreply.github.com>
2025-10-27 11:14:12 +0900

    [Bronze V] Title: X보다 작은 수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/10871. X보다 작은 수/X보다 작은 수.cc
+++ 백준/Bronze/10871. X보다 작은 수/X보다 작은 수.cc
@@ -1,24 +1,22 @@
-// 백준 10871번 : X보다 작은 수
 #include<bits/stdc++.h>
-
 using namespace std;
 
 int main()
 {
 	ios::sync_with_stdio(false);
-	cin.tie(NULL); cout.tie(NULL);
+	cin.tie(nullptr), cout.tie(nullptr);
+
 	int N, X;
 	cin >> N >> X;
 
 	for (int i = 0; i < N; i++)
 	{
-		int A;
-		cin >> A;
-		if (A < X)
+		int val;
+		cin >> val;
+		if (val < X)
 		{
-			cout << A << " ";
+			cout << val << " ";
 		}
 	}
-
 	return 0;
 }

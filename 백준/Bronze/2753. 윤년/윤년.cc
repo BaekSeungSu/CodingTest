7a20132a9d810743eb5f6f358be3831ba46540ee
BaekSeungSu <91333092+BaekSeungSu@users.noreply.github.com>
2025-10-27 11:29:58 +0900

    [Bronze V] Title: 윤년, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2753. 윤년/윤년.cc
+++ 백준/Bronze/2753. 윤년/윤년.cc
@@ -1,23 +1,23 @@
-// 백준 2753번 : 윤년
-#include<bits/stdc++.h>
+// 백준 2753 : 윤년
 
+#include<bits/stdc++.h>
 using namespace std;
 
 int main()
 {
 	ios::sync_with_stdio(false);
-	cin.tie(NULL); cout.tie(NULL);
+	cin.tie(nullptr); cout.tie(nullptr);
 
-	int Year;
-	cin >> Year;
+	int year;
+	cin >> year;
 
-	if ((Year % 4 == 0 && Year % 100 != 0) || Year % 400 == 0)
+	if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0)
 	{
-		cout << 1;
+		cout << "1";
 	}
-	else {
-		cout << 0;
+	else
+	{
+		cout << "0";
 	}
 
-	return 0;
 }
